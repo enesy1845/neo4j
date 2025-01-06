5be8a35e5620bd50230f168b5fcef8e9628ec6da
canbulatb <126672142+canbulatb@users.noreply.github.com>
2025-01-06 18:32:48 +0100

    .

--- routers/questions.py
+++ routers/questions.py
@@ -20,46 +20,50 @@ class AddQuestionRequest(BaseModel):
     points: int
     correct_answer: str
 
+# class AddQuestionResponse(BaseModel):
+#     external_id: uuid4
+
+    
 #AddQuestionResponse
 #QuestionResponse
 
 
 # ========== Endpoints ==========
 
-@router.post("/", response_model=AddQuestionResponse, summary="Add a new question")
-def add_question(
-    body: AddQuestionRequest,
-    db: Session = Depends(get_db),
-    current_user: User = Depends(get_current_user)
-):
-    if current_user.role != "teacher":
-        raise HTTPException(status_code=403, detail="Only teachers can add questions.")
-    if not current_user.registered_section:
-        raise HTTPException(status_code=400, detail="Teacher has no registered section.")
-    if body.q_type not in ['single_choice', 'multiple_choice', 'true_false', 'ordering']:
-        raise HTTPException(status_code=400, detail="Invalid question type.")
+# @router.post("/", response_model=AddQuestionResponse, summary="Add a new question")
+# def add_question(
+#     body: AddQuestionRequest,
+#     db: Session = Depends(get_db),
+#     current_user: User = Depends(get_current_user)
+# ):
+#     if current_user.role != "teacher":
+#         raise HTTPException(status_code=403, detail="Only teachers can add questions.")
+#     if not current_user.registered_section:
+#         raise HTTPException(status_code=400, detail="Teacher has no registered section.")
+#     if body.q_type not in ['single_choice', 'multiple_choice', 'true_false', 'ordering']:
+#         raise HTTPException(status_code=400, detail="Invalid question type.")
 
-    external_id = str(uuid4())
-    section = int(current_user.registered_section)
+#     external_id = str(uuid4())
+#     section = int(current_user.registered_section)
 
-    new_q = Question(
-        external_id=external_id,
-        section=section,
-        question=body.question_text,
-        points=body.points,
-        type=body.q_type
-    )
-    db.add(new_q)
-    db.commit()
-    db.refresh(new_q)
+#     new_q = Question(
+#         external_id=external_id,
+#         section=section,
+#         question=body.question_text,
+#         points=body.points,
+#         type=body.q_type
+#     )
+#     db.add(new_q)
+#     db.commit()
+#     db.refresh(new_q)
 
-    ans = Answer(
-        question_id=new_q.id,
-        correct_answer=body.correct_answer.strip()
-    )
-    db.add(ans)
-    db.commit()
+#     ans = Answer(
+#         question_id=new_q.id,
+#         correct_answer=body.correct_answer.strip()
+#     )
+#     db.add(ans)
+#     db.commit()
 
-    return {"message": "Question added successfully", "external_id": external_id}
+#     return {"message": "Question added successfully", "external_id": external_id}
 
-#list_all_questions - get
+# #list_all_questions - get
